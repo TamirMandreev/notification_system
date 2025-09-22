e71d689033e42c3383de4293d382e27bc6e7ab5b
Tamir Mandreev <tamirmandreev@mail.ru>
2025-09-22 22:38:14 +0800

    Есть модель и представление API для создания объекта модели

--- notifications/models.py
+++ notifications/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-from django.contrib.postgres.fields import ArrayField
 
 class Notification(models.Model):
-    users = ArrayField(models.CharField(max_length=255), default=list, size=4)
+    users = models.JSONField()
     subject = models.CharField(max_length=255)
     body = models.TextField()
