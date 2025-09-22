ce600ab472aafb0c063601b1867ec5a988f8f1ef
Tamir Mandreev <tamirmandreev@mail.ru>
2025-09-22 20:53:57 +0800

    Написал тест создания объекта модели Notification

---
+++ notifications/tests/test_models.py
@@ -0,0 +1,27 @@
+import pytest
+
+from ..models import Notification
+
+
+@pytest.fixture
+def notification():
+    return Notification.objects.create(
+        users = [
+            ['Тамир', 'tamirmandreev@mail.ru', '89915410704',],
+            ['Тимур', 'mandreevts@gmail.com', '+79644111469',]
+        ],
+        subject='Тестовое письмо',
+        body='Проводится тестирование создания объекта модели Notification'
+    )
+
+@pytest.mark.django_db
+def test_notification_creation(notification):
+    '''
+    Тестирует создание объекта модели Notification
+    '''
+    assert notification.users == [
+            ['Тамир', 'tamirmandreev@mail.ru', '89915410704',],
+            ['Тимур', 'mandreevts@gmail.com', '+79644111469',]
+        ]
+    assert notification.subject == 'Тестовое письмо'
+    assert notification.body == 'Проводится тестирование создания объекта модели Notification'
