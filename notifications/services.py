df39e453045ed500b507308ed441fcb97a679034
Tamir Mandreev <tamirmandreev@mail.ru>
2025-09-24 05:35:11 +0800

    Удалил функцию send_users_email из services.py

--- notifications/services.py
+++ notifications/services.py
@@ -6,13 +6,5 @@ def create_users(users):
         User.objects.create(name=user[0], email=user[1], number=user[2])
 
 
-def send_users_email(subject, message,):
-    '''
-    Отправляет каждому пользователю письмо
-    :param subject: str: Тема письма
-    :param message: str: Текст письма
-    '''
-    users = User.objects.all()
-    for user in users:
-        send_async_email.delay(subject, message, user.id, user.email)
+
 
