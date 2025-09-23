a10564b3d578c56f20add4392969b1a94ea56d4e
Tamir Mandreev <tamirmandreev@mail.ru>
2025-09-23 23:45:06 +0800

    Написал тесты для функции send_async_email

---
+++ notifications/tests/test_tasks.py
@@ -0,0 +1,31 @@
+import pytest
+
+from notifications.tasks import send_async_email
+
+from django.core import mail
+
+
+class TestSendAsyncEmail:
+    '''
+    Тестирует функцию send_async_email
+    '''
+
+    def test_send_async_email_success(self):
+        '''
+        Тестирует успешность отправки сообщения
+        :return:
+        '''
+        send_async_email('Test subject', 'Test message', 'tamirmandreev@mail.ru', ['tamirmandreev@mail.ru', 'mandreevts@gmail.com'])
+        assert len(mail.outbox) == 1
+        assert mail.outbox[0].subject == 'Test subject'
+        assert mail.outbox[0].to == ['tamirmandreev@mail.ru', 'mandreevts@gmail.com']
+        assert mail.outbox[0].body == 'Test message'
+        assert mail.outbox[0].from_email == 'tamirmandreev@mail.ru'
+
+    def test_send_async_email_failure(self):
+        '''
+        Тестирует обработку ошибок
+        :return:
+        '''
+        with pytest.raises(Exception):
+            send_async_email('', '', [])
