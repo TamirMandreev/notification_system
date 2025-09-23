a9f6b772cef98cbb92d881307ee360f151d3ea80
Tamir Mandreev <tamirmandreev@mail.ru>
2025-09-24 05:35:58 +0800

    Функция send_async_email из task.py получает всех пользователей и отправляет им письма

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -7,28 +7,26 @@ from notifications.models import EmailSendStatus, User
 
 
 @shared_task
-def send_async_email(subject, message, user_id, user_email):
+def send_async_email(subject, message):
     '''
-    Асинхронно отправляет эл. сообщение через SMTP-сервер
+    Асинхронно отправляет эл. сообщение через SMTP-сервер всем пользователям
     :param subject: тема письма
     :param message: текст письма
-    :param from_email: email адрес отправителя
-    :param recipient_list: список получателей
     :return:
     '''
-    try:
-        result = send_mail(
-            subject=subject,
-            message=message,
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[user_email],
-        )
-        if result > 0:
-            user = User.objects.get(id=user_id)
-            EmailSendStatus.objects.create(user=user, is_successful=True)
-        else:
-            raise Exception('Сообщение не отправлено')
+    users = User.objects.all()
+    for user in users:
+        try:
+            result = send_mail(
+                subject=subject,
+                message=message,
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[user.email],
+            )
+            if result > 0:
+                EmailSendStatus.objects.create(user=user, is_successful=True)
+            else:
+                raise Exception('Сообщение не отправлено')
 
-    except Exception as e:
-        user = User.objects.get(id=user_id)
-        EmailSendStatus.objects.create(user=user, is_successful=False, error_message=str(e))
+        except Exception as e:
+            EmailSendStatus.objects.create(user=user, is_successful=False, error_message=str(e))
