5e94c1971426d037b1e148c3ca46f729040c457f
Tamir Mandreev <tamirmandreev@mail.ru>
2025-09-23 22:52:48 +0800

    Переименовал поле body на message в модели Notificationв

--- notifications/models.py
+++ notifications/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Notification(models.Model):
     users = models.JSONField()
     subject = models.CharField(max_length=255)
-    body = models.TextField()
+    message = models.TextField()
