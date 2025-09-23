7c3ecbfd8b230660732061c9172003b6505cf4a1
Tamir Mandreev <tamirmandreev@mail.ru>
2025-09-23 22:54:41 +0800

    Переопределил метод perform_create представления NotificationCreateAPIView.
    Теперь при создании объекта пользователи получают уведомление

--- notifications/views.py
+++ notifications/views.py
@@ -1,7 +1,21 @@
 from rest_framework.generics import CreateAPIView
+from django.conf import settings
 
 from notifications.serializers import NotificationSerializer
+from notifications.services import get_recipient_list
+
+from notifications.tasks import send_async_email
 
 
 class NotificationCreateAPIView(CreateAPIView):
     serializer_class = NotificationSerializer
+
+    def perform_create(self, serializer):
+        notification = serializer.save()
+
+        recipient_list = get_recipient_list(serializer.validated_data['users'])
+        subject = serializer.validated_data['subject']
+        message = serializer.validated_data['message']
+
+        send_async_email.delay(subject, message, settings.EMAIL_HOST_USER, recipient_list,)
+
