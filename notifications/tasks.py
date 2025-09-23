09f0d3c4e04d4c788a09280e8100188eca6c0e79
Tamir Mandreev <tamirmandreev@mail.ru>
2025-09-23 19:43:21 +0800

    Создал send_async_email для асинхронной отправки сообщений через SMTP-сервер

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -3,9 +3,22 @@ from django.core.mail import send_mail
 from celery import shared_task
 
 @shared_task
-def send_async_email(subject, message, recipient_list):
-    return send_mail(
-        subject=subject,
-        message=message,
-        recipient_list=recipient_list,
-    )
+def send_async_email(subject, message, from_email, recipient_list):
+    '''
+    Асинхронно отправляет эл. сообщение через SMTP-сервер
+    :param subject: тема письма
+    :param message: текст письма
+    :param from_email: email адрес отправителя
+    :param recipient_list: список получателей
+    :return:
+    '''
+    try:
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=from_email,
+            recipient_list=recipient_list,
+        )
+        return f'Email успешно отправлен на {recipient_list}'
+    except Exception as e:
+        return f'Ошибка отправки email: {str(e)}'
